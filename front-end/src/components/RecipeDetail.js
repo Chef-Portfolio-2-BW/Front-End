36fd7e3dbe75a789dd53df8b917cffebb898aeea
AdamMaley <adamemaley@yahoo.com>
2020-02-04 19:52:58 -0500

    Zoomed and team helped fix RecipeDetail.js

--- front-end/src/components/RecipeDetail.js
+++ front-end/src/components/RecipeDetail.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { axiosWithAuth } from './axiosAuth.js';
 
-function RecipeDetail() {
+const RecipeDetail = (props) => {
 
     const [item, setItem] = useState({});
 
     useEffect( () => {
-        axios
-        .get('https://bwchefhub.herokuapp.com/api/recipes/get?ids=${match.params.id}')
-        .then(res => setItem(res.data))
+        axiosWithAuth()
+        .get(`https://bwchefhub.herokuapp.com/api/recipes/${props.match.params.id}`)
+        .then(res => {setItem(res.data) 
+            console.log(res)})
         .catch(err => console.log(err))
-    }, []);
+    }, [props.match.params.id]);
     
     return (
         <div>
